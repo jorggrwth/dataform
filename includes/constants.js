e6567592731739f004ad6c59c94b486994a9da2a
Jorg Bouman <jorg@grwth.amsterdam>
2024-06-03 09:49:07 +0000

    Fixes

--- includes/constants.js
+++ includes/constants.js
@@ -1,6 +1,6 @@
-const GA4_DATABASE = 'civil-zodiac-379017';
-const GA4_DATASET = 'analytics_357800351';
-const GA4_TABLE = 'events_*';
+const GA4_DATABASE = 'still-girder-400914';
+const GA4_DATASET = 'dataform';
+const GA4_TABLE = 'ga4data';
 const START_DATE = "'2024-01-01'";
 
 module.exports = {GA4_DATABASE, GA4_DATASET, GA4_TABLE, START_DATE} 
