2a99d9c9db56a7d4c6a948c338fdfc3c95d2a1e1
Jorg Bouman <jorg@grwth.amsterdam>
2024-06-04 19:24:44 +0000

    Added ga4 logic

--- definitions/sources/ga4/declarations.js
+++ definitions/sources/ga4/declarations.js
@@ -1,5 +1,15 @@
+// const constants = require('./constants');
+
+// Declare UA table
 declare({
     database: constants.UA_DATABASE,
     schema: constants.UA_DATASET,
     name: constants.UA_TABLE,
+});
+
+// Declare GA4 table
+declare({
+    database: constants.GA4_DATABASE,
+    schema: constants.GA4_DATASET,
+    name: constants.GA4_TABLE,
 });
--- includes/constants.js
+++ includes/constants.js
@@ -1,10 +1,19 @@
 const UA_DATABASE = 'still-girder-400914';
 const UA_DATASET = 'dataform';
 const UA_TABLE = 'ga4data';
+
 const GA4_DATABASE = 'still-girder-400914';
 const GA4_DATASET = 'analytics_357800351';
 const GA4_TABLE = 'events_*';
 
 const START_DATE = "'2024-01-01'";
 
-module.exports = {UA_DATABASE, UA_DATASET, UA_TABLE, GA4_DATABASE, GA4_DATASET, GA4_TABLE, START_DATE} 
+module.exports = {
+    UA_DATABASE, 
+    UA_DATASET, 
+    UA_TABLE, 
+    GA4_DATABASE, 
+    GA4_DATASET, 
+    GA4_TABLE, 
+    START_DATE
+};
